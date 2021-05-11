de6f61fc306c510828563ceb3283e1098cfbaaf7
Mathew W. McLean <mathew.w.mclean@gmail.com>
2021-05-11 15:38:06 +1000

    Update 100,000 case regression unit tests
    * Add tests for different missing data options
    * Add test for weighted MNL
    * Add expectations for warnings
    * Addresses code review comments for DS-3290

--- tests/testthat/test-regression100Kcases.R
+++ tests/testthat/test-regression100Kcases.R
@@ -17,21 +17,20 @@ vehicles <- cbind(vehicles, noise)
 
 test_that("Linear regression",
 {
-    expect_is(Regression(log.price~.-region_weight, data = vehicles),
-              "LinearRegression")
     ## Effects Plot doesn't like "." in formula
-    expect_error(print(Regression(log.price ~ drive+condition+log.odometer+region+year,
+    expect_warning(print(Regression(log.price ~ drive+condition+log.odometer+region+year,
                    data = vehicles,
                    weights = region_weight,
-                   output = "Effects Plot")), NA)
-    expect_error(print(Regression(log.price~.-region_weight,
-                   data = vehicles,
-                   weights = region_weight,
-                   output = "ANOVA")), NA)
+                   output = "Effects Plot")),
+                   "Unusual observations detected")
 
-    expect_is(Regression(log.price~.-region_weight, data = vehicles,
-                         weights = region_weight),
-              "LinearRegression")
+    ## Add 15 white noise predictors
+    fit <- Regression(log.price~.-region_weight,
+                   data = vehicles,
+                   weights = region_weight,
+                   output = "ANOVA")
+    expect_is(fit, "LinearRegression")
+    expect_warning(print(fit), "Unusual observations detected")
 })
 
 test_that("Weighted logistic regression", {
@@ -78,6 +77,9 @@ test_that("Multinomial response",
     expect_is(Regression(year~.-region_weight,
                          data = vehicles, type = "Multinomial Logit"),
               "MultinomialLogitRegression")
+    expect_is(Regression(year~.-region_weight, weights = region_weight,
+                         data = vehicles, type = "Multinomial Logit"),
+              "MultinomialLogitRegression")
 
     expect_is(Regression(year~.-region_weight,  ## unweighted
                          data = vehicles, type = "Ordered Logit"),
@@ -121,5 +123,52 @@ test_that("Stepwise",
     fit <- Regression(rformula, data = vehicles)
     expect_is(Stepwise(fit, direction = "Backward"), "Stepwise")
     expect_is(Stepwise(fit, direction = "Forward"), "Stepwise")
+
+})
+
+test_that("Crosstab interaction",
+{
+    rformula <- paste(setdiff(names(vehicles),
+                              c("log.price", "region_weight", "year")),
+                      collapse = "+")
+    rformula <- formula(paste0("log.price~", rformula))
+    fit <- Regression(rformula, data = vehicles, interaction = year)
+    expect_true(fit$test.interaction)
+})
+
+
+test_that("Missing data: Dummy variable adjustment",
+{
+    rformula <- paste(setdiff(names(vehicles),
+                              c("log.price", "region_weight", "year")),
+                      collapse = "+")
+    rformula <- formula(paste0("log.price~", rformula))
+    pred.names <- c("drive", "condition", "log.odometer", "region", "year")
+    n <- nrow(vehicles)
+    prop.missing <- .1
+    set.seed(999)
+    for (i in pred.names)
+        vehicles[[i]][sample(n, ceiling(prop.missing*n))] <- NA
+    expect_is(Regression(rformula, data = vehicles, weight = region_weight,
+                         missing = "Dummy variable adjustment"),
+              "LinearRegression")
+})
+
+test_that("Missing data: Multiple imputation",
+{
+    skip_on_travis()  # Tested by long-running blog test
+    rformula <- paste(setdiff(names(vehicles),
+                              c("log.price", "region_weight", "year")),
+                      collapse = "+")
+    rformula <- formula(paste0("log.price~", rformula))
+    pred.names <- c("drive", "condition", "log.odometer", "region", "year")
+    n <- nrow(vehicles)
+    prop.missing <- .1
+    set.seed(9999)
+    for (i in pred.names)
+        vehicles[[i]][sample(n, ceiling(prop.missing*n))] <- NA
+    expect_is(Regression(rformula, data = vehicles,
+                         missing = "Multiple imputation"),
+              "LinearRegression")
 
 })
