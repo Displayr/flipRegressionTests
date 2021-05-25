4a3a9574b333c56fe96c0cef91655a0fa7bd1f68
Mathew W. McLean <mathew.w.mclean@gmail.com>
2021-05-25 16:04:07 +1000

    Add tests for diagnostics with 100K cases; DS-3318

--- tests/testthat/test-regression100Kcases.R
+++ tests/testthat/test-regression100Kcases.R
@@ -30,7 +30,11 @@ test_that("Linear regression",
                    weights = region_weight,
                    output = "ANOVA")
     expect_is(fit, "LinearRegression")
+    expect_is(predict(fit), "numeric")
     expect_warning(print(fit), "Unusual observations detected")
+
+    ## Regression - Diagnostic - Test Residual Heteroscedasticity
+
 })
 
 test_that("Weighted logistic regression", {
@@ -74,16 +78,23 @@ test_that("Weighted regression with count response", {
 
 test_that("Multinomial response",
 {
+    n <- nrow(vehicles)
+    nlev <- nlevels(vehicles[["year"]])
     expect_is(Regression(year~.-region_weight,
                          data = vehicles, type = "Multinomial Logit"),
               "MultinomialLogitRegression")
-    expect_is(Regression(year~.-region_weight, weights = region_weight,
-                         data = vehicles, type = "Multinomial Logit"),
-              "MultinomialLogitRegression")
+    fit <- Regression(year~.-region_weight, weights = region_weight,
+                         data = vehicles, type = "Multinomial Logit")
+    expect_is(fit, "MultinomialLogitRegression")
+    expect_equal(dim(flipData::Probabilities(fit)), c(n, nlev))
+    expect_equal(nlevels(predict(fit)), nlev)
 
-    expect_is(Regression(year~.-region_weight,  ## unweighted
-                         data = vehicles, type = "Ordered Logit"),
-              "OrderedLogitRegression")
+    fit <- Regression(year~.-region_weight,  ## unweighted
+                         data = vehicles, type = "Ordered Logit")
+    expect_is(fit, "OrderedLogitRegression")
+    expect_equal(dim(flipData::Probabilities(fit)),
+                 c(n, nlev)))
+    expect_equal(nlevels(predict(fit)), nlev)
     expect_is(Regression(year~.-region_weight, weights = region_weight,
                          data = vehicles, type = "Ordered Logit"),
               "OrderedLogitRegression")
@@ -171,4 +182,62 @@ test_that("Missing data: Multiple imputation",
                          missing = "Multiple imputation"),
               "LinearRegression")
 
+})
+
+test_that("Diagnostics",
+{
+    vehicles$great.condition <- factor(as.integer(vehicles$condition == "excellent" |
+                                           vehicles$condition == "like new"))
+    fit <- suppressWarnings(Regression(great.condition~.-region_weight-condition,
+                         weight = region_weight,
+                         data = vehicles, type = "Binary Logit"))
+
+    ## Regression - Save Variable(s) - Probabilities of Each Response
+    expect_equal(dim(flipData::Probabilities(fit)), c(nrow(vehicles), 2))
+    ## Regression - Save Variable(s) - Predicted Values
+    expect_is(predict(fit), "factor")
+    ## Regression - Save Variable(s) - Fitted Values
+    expect_is(fitted(fit), "numeric")
+    ## Regression - Save Variable(s) - Residuals
+    expect_is(resid(fit), "numeric")
+    ## Regression - Diagnostic - Multicollinearity Table (VIF)
+    expect_is(car::vif(fit), "matrix")
+    ## Regression - Diagnostic - Plot - Cook's Distance
+    expect_error(plot(fit, which = 4), NA)
+    ## Regression - Diagnostic - Plot - Cook's Distance vs Leverage
+    expect_error(plot(fit, which = 6), NA)
+    ## Regression - Diagnostic - Plot - Goodness of Fit
+    expect_error(GoodnessOfFitPlot(fit, max.points = 1000), NA)
+    ## Regression - Diagnostic - Plot - Influence Index
+    expect_error(car::influenceIndexPlot(fit,
+                                         id = list(method = 'y', n = 5, cex = 1,
+                                                   location = 'lr'),
+                                         vars = c('Studentized', 'hat', 'Cook')),
+                 NA)
+    ## Regression - Diagnostic - Plot - Normal Q-Q
+    expect_error(plot(fit, which = 2), NA)
+    ## Regression - Diagnostic - Plot - Residuals vs Fitted
+    expect_error(plot(fit, which = 1), NA)
+    ## Regression - Diagnostic - Plot - Residuals vs Leverage
+    expect_error(plot(fit, which = 5), NA)
+    ## Regression - Diagnostic - Plot - Scale-Location
+    expect_error(plot(fit, which = 3), NA)
+    ## Regression - Diagnostic - Prediction-Accuracy Table
+    expect_error(print(ConfusionMatrix(fit)), NA)
+    ## Regression - Diagnostic - Test Residual Normality (Shapiro-Wilk)
+    ##   stats::shapiro.test errors if there are more than 5,000
+    ##    this is because the approximate p-values are not known to be accurate
+    ##    beyond this size (and also b/c the test almost always rejects the null
+    ##   hypothesis with so many samples
+
+    ## Regression - Diagnostic - Test Residual Serial Correlation (Durbin-Watson)
+    expect_equal(DurbinWatson(fit)$p.value, .574, tolerance = 1e-3)
+
+    vehicles$great.condition <- NULL
+
+    ## Regression - Diagnostic - Test Residual Heteroscedasticity
+        ## only for unweighted linear regression, tested above
+    fit <- suppressWarnings(Regression(log.price~.-region_weight,
+                   data = vehicles))
+    expect_equal(car::ncvTest(fit)$p, 0)
 })
